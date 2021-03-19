911f4cc98d74b69e6cae3843b340e83caa06e17d
k_tihonov <k_tihonov@mail.ru>
2021-03-19 18:58:35 +0300

    clean the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import './App.css';
 import axios from 'axios';
 import QuoteCard from './components/QuoteCard'
 import LoadingSpinner from './components/LoadingSpinner';
